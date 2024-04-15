6d9ffb5b271fd49d4d939c20978c0e6b08972c07
Yuyi Pacheco K <yuyik46@gmail.com>
2024-04-14 23:25:07 -0400

    feat: Support overriding configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,24 +7,36 @@ export interface ClientConfiguration {
   environment: Environment;
 }
 
+export interface ClientConfigurationOverrides {
+  baseURL?: string;
+  socketURL?: string;
+}
+
 export class Configuration {
   static default(env: Environment): ClientConfiguration {
     return this.v1(env);
   }
 
-  static v1(env: Environment): ClientConfiguration {
+  static v1(
+    env: Environment,
+    overrides?: ClientConfigurationOverrides
+  ): ClientConfiguration {
     if (env === "staging") {
       return {
-        baseURL: "https://staging.tagshelf.com",
-        socketURL: "",
+        baseURL: overrides?.baseURL
+          ? overrides.baseURL
+          : "https://staging.tagshelf.com",
+        socketURL: overrides?.socketURL ? overrides.socketURL : "",
         version: 1,
         environment: "staging",
       };
     }
 
     return {
-      baseURL: "hthttps://app.tagshelf.com",
-      socketURL: "",
+      baseURL: overrides?.baseURL
+        ? overrides.baseURL
+        : "https://app.tagshelf.com",
+      socketURL: overrides?.socketURL ? overrides.socketURL : "",
       version: 1,
       environment: "production",
     };
